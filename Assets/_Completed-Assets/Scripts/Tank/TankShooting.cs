394864fb130484c23f194b07a0d6c213218aed9d
DESKTOP-PQEKSDR\david <david.carvajal.abellan@gmail.com>
2021-04-27 23:35:12 +0200

    Nueva bala que lanza metralla, falta el trigger que cambie de munición

--- Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -10,6 +10,7 @@ namespace Complete
     {
         public int m_PlayerNumber = 1;              // Used to identify the different players
         public Rigidbody m_Shell;                   // Prefab of the shell
+        public Rigidbody m_Shell_bomb;                   // Prefab of the shell
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned
         public Slider m_AimSlider;                  // A child of the tank that displays the current launch force
         public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source
@@ -22,6 +23,8 @@ namespace Complete
         private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released
         private InputAction m_FireAction;           // Fire Action reference (Unity 2020 New Input System)
         private bool isDisabled = false;            // To avoid enabling / disabling Input System when tank is destroyed
+
+        private bool metralla = true;
 
         private void OnEnable()
         {
@@ -82,13 +85,25 @@ namespace Complete
             // Create an instance of the shell and store a reference to it's rigidbody
             Rigidbody shellInstance;
 
-            shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            if(metralla){
 
-            // Set the shell's velocity to the launch force in the fire position's forward direction
-            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+                shellInstance = Instantiate(m_Shell_bomb, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
-            NetworkServer.Spawn(shellInstance.gameObject);
+                // Set the shell's velocity to the launch force in the fire position's forward direction
+                shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
 
+                NetworkServer.Spawn(shellInstance.gameObject);
+
+            }else{
+
+                shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+                // Set the shell's velocity to the launch force in the fire position's forward direction
+                shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+                NetworkServer.Spawn(shellInstance.gameObject);
+
+            }
             // Reset the launch force.  This is a precaution in case of missing button events
             m_CurrentLaunchForce = m_MinLaunchForce;
         }
